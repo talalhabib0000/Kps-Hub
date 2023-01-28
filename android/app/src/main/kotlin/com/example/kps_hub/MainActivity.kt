0ceebca5288edf38ba44b1ed947c083ec63fe103
talalhabib <talalhabib0000@outlook.com>
2023-01-28 20:42:03 +0500

    setting orientation

---
+++ android/app/src/main/kotlin/com/example/kps_hub/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.kps_hub
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+
+import 'screens/screens.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await SystemChrome.setPreferredOrientations(<DeviceOrientation>[
+    DeviceOrientation.portraitUp,
+    DeviceOrientation.portraitDown
+  ]).then((_) => runApp(const KPSHub()));
+}
+
+class KPSHub extends StatelessWidget {
+  const KPSHub({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        title: 'KPS Hub',
+        theme: ThemeData(
+          primarySwatch: Colors.blue,
+        ),
+        home: const HomeScreen());
+  }
+}
